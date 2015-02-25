a12582fbc850184c2ebe906b905043f10d636706
amayzel <avigaylmayzel94@gmail.com>
2015-02-24 23:46:06 -0500

    compiler altered reads from file

--- src/mayzel/compiler/Compiler.java
+++ src/mayzel/compiler/Compiler.java
@@ -1,121 +1,124 @@
 package mayzel.compiler;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Compiler {
 
 	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		StringBuilder info = new StringBuilder();
-		String [] commands = new String[256];
-		String command = "";
-		int location;
-		int counter= 0;
-		String temp;
-		do{
-			command = keyboard.next();
-			switch(command){
+		try {
+			Scanner file = new Scanner(new File("./file.txt"));
+			String[] commands = new String[256];
+			String command = "";
+			int location;
+			int counter = 0;
+			String temp;
+
+			for (int i = 0; i < commands.length; i++) {
+				commands[i] = "0";
+			}
+
+			do {
+				command = file.next();
+				switch (command) {
 				case "//":
-					keyboard.nextLine();
+					file.nextLine();
 					break;
 				case "LD":
 					commands[counter++] = "0";
-					location = keyboard.nextInt();
+					location = file.nextInt();
 					temp = Integer.toHexString(location);
-					if(temp.length()>1){
+					if (temp.length() > 1) {
 						commands[counter++] = String.valueOf(temp.charAt(0));
 						commands[counter++] = String.valueOf(temp.charAt(1));
-					}
-					else{
-						commands[counter++] = "0";	
+					} else {
+						commands[counter++] = "0";
 						commands[counter++] = String.valueOf(temp.charAt(0));
 					}
-					keyboard.nextLine();
+					file.nextLine();
 					break;
 				case "ST":
 					commands[counter++] = "1";
-					location = keyboard.nextInt();
+					location = file.nextInt();
 					temp = Integer.toHexString(location);
-					if(temp.length()>1){
+					if (temp.length() > 1) {
 						commands[counter++] = String.valueOf(temp.charAt(0));
 						commands[counter++] = String.valueOf(temp.charAt(1));
-					}
-					else{
-						commands[counter++] = "0";	
+					} else {
+						commands[counter++] = "0";
 						commands[counter++] = String.valueOf(temp.charAt(0));
 					}
-					keyboard.nextLine();
+					file.nextLine();
 					break;
 				case "SWP":
 					commands[counter++] = "2";
-					keyboard.nextLine();
+					file.nextLine();
 					break;
 				case "ADD":
 					commands[counter++] = "3";
-					keyboard.nextLine();
+					file.nextLine();
 					break;
 				case "INC":
 					commands[counter++] = "4";
-					keyboard.nextLine();
+					file.nextLine();
 					break;
 				case "DEC":
 					commands[counter++] = "5";
-					keyboard.nextLine();
+					file.nextLine();
 					break;
 				case "BZ":
 					commands[counter++] = "6";
-					location = keyboard.nextInt();
+					location = file.nextInt();
 					temp = Integer.toHexString(location);
-					if(temp.length()>1){
+					if (temp.length() > 1) {
 						commands[counter++] = String.valueOf(temp.charAt(0));
 						commands[counter++] = String.valueOf(temp.charAt(1));
-					}
-					else{
-						commands[counter++] = "0";	
+					} else {
+						commands[counter++] = "0";
 						commands[counter++] = String.valueOf(temp.charAt(0));
 					}
-					keyboard.nextLine();
+					file.nextLine();
 					break;
 				case "BR":
 					commands[counter++] = "7";
-					location = keyboard.nextInt();
+					location = file.nextInt();
 					temp = Integer.toHexString(location);
-					if(temp.length()>1){
+					if (temp.length() > 1) {
 						commands[counter++] = String.valueOf(temp.charAt(0));
 						commands[counter++] = String.valueOf(temp.charAt(1));
-					}
-					else{
-						commands[counter++] = "0";	
+					} else {
+						commands[counter++] = "0";
 						commands[counter++] = String.valueOf(temp.charAt(0));
 					}
-					keyboard.nextLine();
+					file.nextLine();
 					break;
 				case "STP":
 					commands[counter++] = "8";
-					keyboard.nextLine();
+					file.nextLine();
 					break;
 				case "DATA":
-					int where = keyboard.nextInt();
-					String what = Integer.toHexString(keyboard.nextInt());
-					if(what.length()>1){
+					int where = file.nextInt();
+					String what = Integer.toHexString(file.nextInt());
+					if (what.length() > 1) {
 						commands[where++] = String.valueOf(what.charAt(0));
 						commands[where] = String.valueOf(what.charAt(1));
-					}
-					else{
-						commands[where++] = "0";	
+					} else {
+						commands[where++] = "0";
 						commands[where] = String.valueOf(what.charAt(0));
 					}
-					keyboard.nextLine();
+					file.nextLine();
 					break;
-			}	
-		}while(keyboard.hasNext());//wrong cuz its input
+				}
+			} while (file.hasNext());
 
-		for (int i = 0; i < commands.length; i++) {
-			if(commands[i].equals(null)){
-				commands[i] = "0";
+			for (int i = 0; i < commands.length; i++) {
+				System.out.print(commands[i].toUpperCase());
 			}
-			System.out.print(commands[i].toUpperCase());
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-		
+
 	}
 }
